1d16f4745d8c5a3887fcbfe759e74c5e37c16b6b
jrlapayne <jrlapayne@gmail.com>
2012-12-10 12:45:32 -0600

    for real the fb login works now

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :facebook, '282324831851627', 'ce4aaeb4e63d13ed2e6adea6eb354181'  #localhost
-  provider :facebook, '430430590318322', '5856d98c593b31655690a08889d46305'  #high-lightning-4791
+  provider :facebook, '336449143129017', 'ee6de69f67830a4d75002642f1e8458c'  #high-lightning-4791
 end
